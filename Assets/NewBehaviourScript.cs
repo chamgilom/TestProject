2910396c3c95ab247bf79ac755619529c70c2d8b
chamgilom <149364808+chamgilom@users.noreply.github.com>
2023-12-01 16:26:18 +0900

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private void Awake()//초기화 1
+    {
+        Debug.Log("초기화 1");
+    }
     // Start is called before the first frame update
-    void Start()
+    void Start()// 초기화 2
+    {
+        Debug.Log("초기화 2");
+    }
+    private void FixedUpdate()//물리연산 끝나고 호출
+    {
+
+    }
+        // Update is called once per frame
+    void Update()// 매 1프레임마다 호출
+    {
+
+    }
+    private void LateUpdate()//위에 애들 다 끝나고 호출
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()//게임오브젝트가 켜질때
     {
-        
+        Debug.Log("게임 오브젝트 켜짐");
+    }
+    private void OnDisable()//게임오브젝트가 꺼질때
+    {
+        Debug.Log("게임 오브젝트 꺼짐");
     }
 }
